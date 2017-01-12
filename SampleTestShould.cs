e56d9aa9a128b4ab40db44796141f46ff6d09f54
Modesto San Juan <modestosja@gmail.com>
2017-01-13 00:05:35 +0100

    Sample test

---
+++ SampleTestShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace OhceKata
+{
+    public class SampleTestShould
+    {
+        [Fact]
+        public void Pass()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
